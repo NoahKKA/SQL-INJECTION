01bf5a6e3f5c53e98b049ded8e350fbcb0e073f2
NoahKKA <noahhhhh.a@gmail.com>
2023-07-24 17:40:57 -0700

    logging in with correct username and wrong password does not work

--- app.js
+++ app.js
@@ -12,20 +12,19 @@ const db = new sqlite3.Database(":memory:");
 db.serialize(function () {
   db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
   db.run(
-    "INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')",
-    "INSERT INTO user VALUES ('username', 'password', 'Administrator')"
+    "INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')"
   );
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join("index.html"));
+  res.sendFile("index.html");
 });
 
 app.post("/login", (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
   
-    let query = `SELECT title FROM user WHERE username = '${username}' AND password = '${password}'`;
+    let query = "SELECT title FROM user where username = '" + username + "' and password = '" + password + "'";
   
     console.log(
       `
